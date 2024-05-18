dc22aa58e6ee6fdc5c98d53c138ed0936cf15218
AgustinVillavicencio <villavicencio.agustin.17@gmail.com>
2024-05-18 12:38:42 -0300

    submenu articulos

--- FrontEnd_React/src/Components/NavBar.tsx
+++ FrontEnd_React/src/Components/NavBar.tsx
@@ -1,24 +1,51 @@
-import { Link } from "react-router-dom"
-export default function NavBar(){
-    return(
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+    const [isArticulosOpen, setIsArticulosOpen] = useState(false);
+
+    const ArticulosMenu = () => {
+        setIsArticulosOpen(!isArticulosOpen);
+    };
+
+    return (
         <nav className="navBarContainer">
             <ul className="listaNavBar">
-              <li>
-                <Link to={`/`} className="itemNavBar">
-                    Inicio
-                </Link>
-              </li>
-              <li>
-                <Link to={`/articulos`} className="itemNavBar">
-                    Articulos
-                </Link>
-              </li>
-              <li>
-                <Link to={`/articulos/categorias`} className="itemNavBar">
-                    Categorias
-                </Link>
-              </li>
+                <li>
+                    <Link to={`/`} className="itemNavBar">
+                        Inicio
+                    </Link>
+                </li>
+                <li>
+                    <Link to={`/empresas`} className="itemNavBar">
+                        Empresa
+                    </Link>
+                </li>
+                <li>
+                    <div className="itemNavBar" onClick={ArticulosMenu}>
+                        Artículos
+                    </div>
+                    {isArticulosOpen && (
+                        <ul className="subMenu">
+                            <li>
+                                <Link to={`/articulos`} className="itemNavBar">
+                                    Lista de artículos
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to={`/articulos/categorias`} className="itemNavBar">
+                                    Categorías
+                                </Link>
+                            </li>
+                        </ul>
+                    )}
+                </li>
+                <li>
+                    <Link to={`/promociones`} className="itemNavBar">
+                        Promociones
+                    </Link>
+                </li>
             </ul>
         </nav>
-    )
+    );
 }
